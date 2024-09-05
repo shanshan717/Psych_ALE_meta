b8b809e13720c5b22b7709fab899db9b46002fda
ss <126754586+shanshan717@users.noreply.github.com>
2024-09-05 09:11:44 +0800

    update

--- scripts/02_HCs.py
+++ scripts/02_HCs.py
@@ -47,8 +47,7 @@ def run_ale(text_file, voxel_thresh, cluster_thresh, random_seed, n_iters, outpu
 
     # Perform the ALE
     # target默认的是'ale_2mm'和'mni152_2mm'两种，这里我们选择'mni152_2mm'
-    text_file_path = os.path.abspath('data/health.txt')
-    dset = io.convert_sleuth_to_dataset(text_file=text_file_path, target="mni152_2mm") 
+    dset = io.convert_sleuth_to_dataset(text_file="data/health.txt", target="mni152_2mm") 
     ale = meta.cbma.ALE()
     res = ale.fit(dset)
 
@@ -82,7 +81,7 @@ def run_ale(text_file, voxel_thresh, cluster_thresh, random_seed, n_iters, outpu
 # 运行ALE分析
 if __name__ == "__main__":
     # 输入疾病患者的Sleuth文件
-    sleuth_file = "data/health.txt"  
+    sleuth_file = "data/health.txt"
 
     # 应用我们定义的 run_ale 函数
     cres = run_ale(
@@ -91,7 +90,7 @@ if __name__ == "__main__":
         cluster_thresh=0.05,
         random_seed=1234,
         n_iters=5000,  
-        output_dir="../output/",
+        output_dir="../output",
     )
 
 
